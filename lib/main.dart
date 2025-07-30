bcd8583e04bf89209803c36cf5c8b6d3b6ed522f
KWONHYUKJIN <insort@naver.com>
2025-07-30 13:14:16 +0900

    Gemini: Add webview with mocknote.com and enable hot reload for URL changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (WebViewPlatform.instance is AndroidWebViewPlatform) {
+    AndroidWebViewPlatform.registerWith();
+  } else if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+    WebKitWebViewPlatform.registerWith();
+  }
+
   runApp(const MyApp());
 }
 
@@ -13,16 +22,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // 여기에 URL을 전달합니다. 이 URL을 변경하고 핫 리로드하면 웹뷰가 업데이트됩니다.
+      home: const MyHomePage(title: 'Mocknote Webview', url: 'http://mocknote.com'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title, required this.url});
 
   final String title;
+  final String url; // 새로운 URL 매개변수
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -30,31 +43,69 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late final WebViewController _controller;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
+
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
       ..setNavigationDelegate(
         NavigationDelegate(
           onProgress: (int progress) {
-            // Update loading bar.
+            debugPrint('WebView loading progress: $progress%');
           },
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onWebResourceError: (WebResourceError error) {},
+          onPageStarted: (String url) {
+            setState(() {
+              _isLoading = true;
+            });
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              _isLoading = false;
+            });
+            debugPrint('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('웹 리소스 에러: ${error.description}');
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('페이지 로딩 중 오류 발생: ${error.description}')),
+            );
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
         ),
-      )
-      ..loadRequest(Uri.parse('https://mocknote.com'));
+      );
+    // 초기 URL 로드
+    _controller.loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  void didUpdateWidget(covariant MyHomePage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // 위젯의 URL이 변경되면 웹뷰를 다시 로드합니다.
+    if (widget.url != oldWidget.url) {
+      _controller.loadRequest(Uri.parse(widget.url));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: WebViewWidget(controller: _controller),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          if (_isLoading)
+            const Center(child: CircularProgressIndicator()),
+        ],
+      ),
     );
   }
 }
